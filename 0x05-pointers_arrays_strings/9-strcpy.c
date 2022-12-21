8f79551f83b85bedf4480fdb6f06be590b8acea7
Eric Mensah <osamensaheric@gmail.com>
2022-12-20 20:46:55 -0800

    Copies the string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string
+ * @dest: points to a buffer
+ * @src: points to a string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
