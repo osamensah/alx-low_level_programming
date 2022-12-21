bca562c10d7736ebf60d889654e3af2f0a688bfb
Eric Mensah <osamensaheric@gmail.com>
2022-12-20 19:06:24 -0800

    Change return statement

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: - variable of type str
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	return (i);
+}
