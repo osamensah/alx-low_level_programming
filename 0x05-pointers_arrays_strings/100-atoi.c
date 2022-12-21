afd66b1495d7a5d37ab77a9864a3e51487ff9aac
Eric Mensah <osamensaheric@gmail.com>
2022-12-20 21:03:40 -0800

    Removed an

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 /**
  * _atoi - converts a string to an integer
  *
- * @s: string to be converted to an integer
+ * @s: string to be converted to integer
  * Return: integer
  */
 int _atoi(char *s)
