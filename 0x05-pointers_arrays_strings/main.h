eba27e35021c7c517d0f8b9f4da0424c6fc4050d
Eric Mensah <osamensaheric@gmail.com>
2022-12-20 18:44:13 -0800

    Change function

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H>
+#define MAIN_H
 
 #include <stdio.h>
 #include <string.h>
@@ -12,8 +12,9 @@ int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-int _atoi(char *s);
+void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 
 #endif
